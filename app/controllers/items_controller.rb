b28ed9c49d63a8d749374bcb8a62eeff5bda1a12
Andrew Speth <speth.andrew@yahoo.com>
2022-05-09 20:46:34 -0600

    feat: items index page can create new items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,20 @@ class ItemsController < ApplicationController
   def index
     @store = Store.find(params[:store_id])
   end
+  
+  def new
+    @store = Store.find(params[:store_id])
+  end
+  
+  def create
+    @store = Store.find(params[:store_id])
+    @store.items.create!(item_params)
+    redirect_to "/stores/#{@store.id}/items"
+  end
+
+  private
+
+    def item_params
+      params.permit(:name, :price)
+    end
 end
