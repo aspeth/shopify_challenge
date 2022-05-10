54edcbc1f3b25b76024ed0f72f0d42b44fdd2a3e
Andrew Speth <speth.andrew@yahoo.com>
2022-05-09 20:15:41 -0600

    test: items index page shows all items from current store

---
+++ spec/features/items/index_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "items index page" do
+  it "shows all items from the current store" do
+    #store and items we will expect to see
+    shoes = Store.create!(name: "Soren's Shoes")
+    left_shoe = Item.create!(name: "Left shoe", price: 100, store_id: shoes.id)
+    right_shoe = Item.create!(name: "Right shoe", price: 100, store_id: shoes.id)
+    
+    #store and items we will expect to NOT see
+    castles = Store.create!(name: "Carl's Castles")
+    cat_castle = Item.create!(name: "A castle for a cat", price: 300, store_id: castles.id)
+
+    visit "/stores/#{shoes.id}/items"
+
+    #we should see items from the current store
+    expect(page).to have_content("Soren's Shoes")
+    expect(page).to have_content("Left shoe")
+    expect(page).to have_content("Right shoe")
+    
+    #we should NOT see items from another store
+    expect(page).to_not have_content("A castle for a cat")
+  end
+end
