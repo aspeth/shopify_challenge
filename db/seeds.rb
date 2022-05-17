6c77ad85aa027d47589943718d435365cf6389db
Andrew Speth <speth.andrew@yahoo.com>
2022-05-17 08:40:22 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    shoes = Store.create!(name: "Soren's Shoes")
+    left_shoe = Item.create!(name: "Left shoe", price: 100, store_id: shoes.id, quantity: 10)
+    right_shoe = Item.create!(name: "Right shoe", price: 200, store_id: shoes.id, quantity: 10)
